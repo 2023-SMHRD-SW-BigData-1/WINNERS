637f5113a9d1c1114fee630a17ee75fecda9b018
smhrd <smhrd@DESKTOP-DMQG2PT>
2023-05-09 11:26:49 +0900

    10팩토리얼 커밋

--- GItTest/src/Ex02.java
+++ GItTest/src/Ex02.java
@@ -4,6 +4,12 @@ public class Ex02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("안녕하세용옹옹");
+        
+        int num =1 ;
+        for(int i = 0; i<10; i++) {
+        	num *= (i+1); 
+        }
+        System.out.println("10팩토리얼 : "+num);
 	}
 
 }
